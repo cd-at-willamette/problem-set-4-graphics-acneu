1a214763441b442c1512928c8a7e3f65cc99b0f5
acneu <acneu@willamette.edu>
2024-10-13 16:30:29 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -1,7 +1,7 @@
 ########################################
-# Name:
-# Collaborators:
-# Estimated time spent (hrs):
+# Name: Anika Neu
+# Collaborators: Quad Center
+# Estimated time spent (hrs): 1
 ########################################
 
 from pgl import GWindow, GRect
@@ -19,24 +19,20 @@ def draw_pyramid():
 
     gw = GWindow(WIDTH, HEIGHT)
 
-    # You got it from here
+# Defining the rectangle's dimensions and making sure the outline of the bricks is black and the middle is clear
+    def my_rect(x,y,w,h): 
+        rect = GRect(x,y,w,h)
+        rect.set_filled(False)
+        gw.add(rect)
 
+# Creating the bricks that are centered in the middle of the screen, go down by one when counting from the bottom row to the top, and have them vary by half a brick
+    def n_bricks():
+        for y in range(BRICKS_IN_BASE):
+            for x in range(1, y+2):
+                my_rect(((WIDTH/2 - BRICK_WIDTH/2) - y * BRICK_WIDTH/2 + x * BRICK_WIDTH - BRICK_WIDTH), ((HEIGHT/2 - (BRICKS_IN_BASE/2 * BRICK_HEIGHT)) + BRICK_HEIGHT * y), BRICK_WIDTH, BRICK_HEIGHT)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Calling the function
+    n_bricks()
 
 if __name__ == '__main__':
     draw_pyramid()
