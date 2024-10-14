14c99ff9adba22c71d225cc2dc86e0cd890f7bc4
acneu <acneu@willamette.edu>
2024-10-13 17:25:45 -0700

    Update Prob3.py

--- Prob3.py
+++ Prob3.py
@@ -1,7 +1,7 @@
 ########################################
-# Name:
-# Collaborators:
-# Estimate time spent (hrs):
+# Name: Anika Neu
+# Collaborators: Quad Center & Deidrik Boberg
+# Estimate time spent (hrs): 2
 ########################################
 
 from pgl import GWindow, GRect, GLabel, GLine
@@ -14,28 +14,47 @@ SCORE_DX = 10                       # Distance from left of window to origin
 SCORE_DY = 10                       # Distance up from bottom of window to baseline
 SCORE_FONT = "bold 40pt 'serif'"    # Font for score
 
+
 def clicky_box():
-
-    # Defining the callback function, which you won't need until Part C
+# The mouse location and what happens when the box is pressed 
     def on_mouse_down(event):
-        print("You clicked the window!") # Delete this once you start Part C
-
-
-    # Down here you should initialize the window and draw the initial square
-    # Make sure you tab it in so that it is part of the clicky_box function
+        x = squ.get_x()
+        y = squ.get_y()
+ # x and y coordinate of the square
+        xs = random.randint(0, GW_WIDTH - SQUARE_SIZE)
+        ys = random.randint(0, GW_HEIGHT - SQUARE_SIZE)
+# x and y coordinate of the mouse
+        xm = event.get_x()
+        ym = event.get_y()
+# Comparison between the two
+        if xm >= x and xm <= x + SQUARE_SIZE and ym >= y and ym <= y + SQUARE_SIZE:
+            squ.set_location(xs, ys)
+            gw.count += 1
+            label.set_label(str(gw.count))
+# Label resets to 0 if the square is not clicked
+        else:
+            gw.count = 0
+            label.set_label(str(gw.count))
 
     gw = GWindow(GW_WIDTH, GW_HEIGHT)
 
+# Making my square
+    xc = GW_WIDTH/2 - SQUARE_SIZE/2
+    yc = GW_HEIGHT/2 - SQUARE_SIZE/2
+# Setting my square color and location
+    squ = GRect(xc, yc, SQUARE_SIZE, SQUARE_SIZE)
+    squ.set_color('Pink')
+    squ.set_filled(True)
+    gw.add(squ)
 
+# Adding my label
+    gw.count = 0
+    label = GLabel(str(gw.count), SCORE_DX, GW_HEIGHT - SCORE_DY)
+    label.set_font(SCORE_FONT)
+    gw.add(label)
 
-
-
-
-
-
-
-
-
+# Call mouse down function with the click
+    gw.add_event_listener("click", on_mouse_down)
 
 if __name__ == '__main__':
     clicky_box()
